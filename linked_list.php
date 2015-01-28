a49518564d80b024af698e941626956bcdc18212
Casey LaRue <casey.larue@gmail.com>
2015-01-28 13:03:27 -0800

    added rfill group assignment

---
+++ linked_list.php
@@ -0,0 +1,20 @@
+<?php
+
+class DoublyLinkedNode
+{
+	// add a new note
+
+	// delete existing node
+
+	// insert a new node
+
+	// print out the values in the linked list
+}
+
+class Node
+{
+	// necessary properties for each note
+
+}
+
+?>
